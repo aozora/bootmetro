72ebb55241bc4b0c552d557e695cc8082f694586
aozora <marcello.palmitessa@gmail.com>
2013-04-07 16:12:46 +0200

    wip

--- build/index.js
+++ build/index.js
@@ -11,36 +11,12 @@ var  hogan = require('hogan.js')
    , robots_index = '<meta name="robots" content="index, nofollow">'
 
 
-var demopages = [ 'application-bar',
-                  'hub',
-                  'list-detail',
-                  'listview',
-                  'metro-layouts',
-                  'table',
-                  'tiles-templates',
-                  'square-tiles-templates',
-                  'wide-tiles-templates',
-                  'widepeek-tiles-templates'
-                ];
-var docpages = [ 'index-doc',
-             'scaffolding',
-             'base-css',
-             'components',
-             'javascript',
-             'metro-components',
-             'pivot',
-             'toast'
-           ];
+var templates = [__dirname + '/../templates/demo',
+                 __dirname + '/../templates/docs',
+                 __dirname + '/../templates/pages']
 
 
 var layout, pages, sidebar, sitemenu, partial_loggeduser, partial_charms, partial_headermenu
-
-// compile layout template
-layout = fs.readFileSync(__dirname + '/../templates/layout.mustache', 'utf-8')
-layout = hogan.compile(layout, { sectionTags:[
-   {o:'_i', c:'i'}
-] })
-
 
 // retrieve partials
 partial_loggeduser = fs.readFileSync(__dirname + '/../templates/partials/logged-user.mustache', 'utf-8')
@@ -59,56 +35,77 @@ partial_headermenu = hogan.compile(partial_headermenu, { sectionTags:[
 ] })
 
 
-// retrieve pages
-pages = fs.readdirSync(__dirname + '/../templates/pages')
 
-// iterate over pages
-pages.forEach(function (name) {
 
-   // exclude non mustache files
-   if (!name.match(/\.mustache$/))
-      return
 
-   // exclude files that start with "_"
-   if (name.substring(0, 1) == '_')
-      return
 
-   var page = fs.readFileSync(__dirname + '/../templates/pages/' + name, 'utf-8')
-      , context = {}
+// cycle for each template dir
+templates.forEach(function(templatedir){
 
-   context[name.replace(/\.mustache$/, '')] = 'active'
-   context._i = true
-   context.production = prod
-   context.appname = appname
-   context.title = name.replace(/\.mustache/, '')
-                       .replace(/\-.*/, '')
-                       .replace(/(.)/, function ($1) {
-                           return $1.toUpperCase()
-                        })
+   // compile layout template
+   layout = fs.readFileSync(templatedir + '/_layout.mustache', 'utf-8')
+   layout = hogan.compile(layout)
 
-   if (context.title == 'Index') {
-      context.title = title
-      context.robots = robots_index 
-   } else {
-      context.title += ' · ' + title
-      context.robots = robots_noindex
-   }
+   // retrieve pages
+   pages = fs.readdirSync(templatedir)
 
-   page = hogan.compile(page, { sectionTags:[
-      {o:'_i', c:'i'}
-   ] })
 
-   page = layout.render(context, {
-      body:page,
-      charms:partial_charms,
-      loggeduser:partial_loggeduser,
-      headermenu:partial_headermenu
+
+   // iterate over pages
+   pages.forEach(function (name) {
+
+      // exclude non mustache files
+      if (!name.match(/\.mustache$/))
+         return
+
+      // exclude files that start with "_"
+      if (name.substring(0, 1) == '_')
+         return
+
+      var page = fs.readFileSync(templatedir + '/' + name, 'utf-8')
+         ,context = {}
+
+      context[name.replace(/\.mustache$/, '')] = 'active'
+      context._i = true
+      context.production = prod
+      context.appname = appname
+      context.title = name.replace(/\.mustache/, '')
+                          .replace(/\-.*/, '')
+                          .replace(/(.)/, function ($1) {
+                              return $1.toUpperCase()
+                           })
+
+      page = hogan.compile(page, { sectionTags:[
+         {o:'_i', c:'i'}
+      ] })
+
+      page = layout.render(context, {
+         body: page,
+         charms: partial_charms,
+         loggeduser: partial_loggeduser,
+         headermenu: partial_headermenu
+      })
+
+
+      var destinationPath
+      if ( templatedir.match(/pages$/) ){
+         destinationPath = __dirname + '/../';
+      } else if ( templatedir.match(/docs$/) ){
+         destinationPath = __dirname + '/../docs/'
+      } else {
+         // demo & docs
+         destinationPath = __dirname + '/../demo/'
+      }
+
+      console.log(destinationPath)
+
+      //fs.writeFileSync(destinationPath + name.replace(/mustache$/, 'html'), page, 'utf-8')
    })
 
-
-   var destinationPath = __dirname + '/../';
-   fs.writeFileSync(destinationPath + name.replace(/mustache$/, 'html'), page, 'utf-8')
 })
+
+
+
 
 
 // compile LESS styles
