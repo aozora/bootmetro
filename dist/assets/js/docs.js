5dedc90c3c4a79641dcafa8e38043400f83386e7
aozora <marcello.palmitessa@gmail.com>
2013-04-18 17:56:39 +0200

    charts in pivot

--- dist/assets/js/docs.js
+++ dist/assets/js/docs.js
@@ -90,10 +90,32 @@
    });
 
 
-//     // Datepicker demo
-//     $('.datepicker').datepicker();
-//
-//     $('#dp3').datepicker();
+      // charts demo
+      //Get context with jQuery - using jQuery's .get() method.
+      var ctx = $("#myChart").get(0).getContext("2d");
+      //This will get the first returned node in the jQuery collection.
+      var myNewChart = new Chart(ctx);
+
+     var data = {
+        labels : ["January","February","March","April","May","June","July"],
+        datasets : [
+           {
+              fillColor : "rgba(220,220,220,0.5)",
+              strokeColor : "rgba(220,220,220,1)",
+              pointColor : "rgba(220,220,220,1)",
+              pointStrokeColor : "#fff",
+              data : [65,59,90,81,56,55,40]
+           },
+           {
+              fillColor : "rgba(151,187,205,0.5)",
+              strokeColor : "rgba(151,187,205,1)",
+              pointColor : "rgba(151,187,205,1)",
+              pointStrokeColor : "#fff",
+              data : [28,48,40,19,96,27,100]
+           }
+        ]
+     };
+     new Chart(ctx).Line(data,{});
 
   });
 
