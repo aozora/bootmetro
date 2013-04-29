734da4f9cfba065beedfcc2c07077005b40d66b7
aozora <marcello.palmitessa@gmail.com>
2013-04-29 17:53:47 +0200

    wip

--- dist/assets/js/demo.js
+++ dist/assets/js/demo.js
@@ -71,6 +71,34 @@ function updateCSS(css){
 
 
 
+/* Win8 animation */
+function showDashBoard(){
+   for(var i = 1; i <= 3; i++) {
+      $('.col'+i).each(function(){
+         $(this).addClass('fadeInForward-'+i).removeClass('fadeOutback');
+      });
+   }
+}
+
+function fadeDashBoard(){
+   for(var i = 1; i <= 3; i++) {
+      $('.col'+i).addClass('fadeOutback').removeClass('fadeInForward-'+i);
+   }
+}
+
+$('.big, .small').each(function(){
+   var $this= $(this),
+      page = $this.data('page');
+   $this.on('click',function(){
+      $('.page.'+page).addClass('openpage');
+      fadeDashBoard();
+   })
+});
+
+
+
+
+
 // NOTICE!! DO NOT USE ANY OF THIS JAVASCRIPT
 // IT'S ALL JUST JUNK FOR OUR DOCS!
 // ++++++++++++++++++++++++++++++++++++++++++
